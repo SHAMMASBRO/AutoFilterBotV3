b28a7837dcf1df7e5e4f57ca7556c6828885dcd8
SHAMMASBRO <116548675+SHAMMASBRO@users.noreply.github.com>
2024-11-23 07:43:24 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,24 +1,24 @@
 # Bot information
-SESSION = 'LuciferMoringstar_Robot'
+SESSION = 'CkFilterdBot'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:LuciferMoringstar_Robot-zyx57W2v1u123ew11'
-USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
+API_ID = 27326387
+API_HASH = 'bde5a81c08ee3341ee953b714d297534'
+BOT_TOKEN = '7422485332:AAFN3TdvIfpVLvPqYO0GkESV5NmXzBGIIt4'
+USERBOT_STRING_SESSION = 'CkFilterdBot'
 
 # Bot settings
 CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 987654321]
-CHANNELS = [-10012345678, -100987654321, 'Mo_Tech_YT']
+ADMINS = [6639622980, 'admin123', 6639622980]
+CHANNELS = [-1002063643170, -1002085370360, 'Shammas']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[LuciferMoringstar_Robot:LuciferMoringstar_Robot@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'LuciferMoringstar_Robot'
+DATABASE_URI = "mongodb+srv://SM:SM@cluster0.xvvtb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_NAME = 'CkFilterdBot'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
@@ -29,4 +29,4 @@ Here you can search files in inline mode. Just press follwing buttons and start 
 """
 
 SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
-INVITE_MSG = 'Please join @.... to use this bot'
+INVITE_MSG = 'Please join @Cinemakandalo to use this bot'
